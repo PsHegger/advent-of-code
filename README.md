0782e4cfe53fe014d8d21e5eadee31ba737593f6
pshegger <pshegger@gmail.com>
2024-05-17 09:03:25 +0200

    updated kotlin version

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Advent of Code is an advent calendar of programming puzzles.
 This repository contains my solutions for them.
 
-You can read more about Advent of Code at it's website [https://adventofcode.com/](https://adventofcode.com/)
+You can read more about Advent of Code at its website [https://adventofcode.com/](https://adventofcode.com/)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.9.24"
     application
 }
 
@@ -24,7 +24,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 tasks.withType<JavaExec> {
